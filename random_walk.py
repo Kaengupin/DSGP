a4e5fd28f54b204929333cc340f85b98e48fb83e
wedemanr <r-wedemann@web.de>
2020-02-14 12:05:29 +0100

    correct random walk solutions

--- random_walk.py
+++ random_walk.py
@@ -15,50 +15,54 @@ try:
 except:
     pass
 
-dt = 2**(-4)
-n=int(1/dt)+1
 
-dW = np.random.normal(0,1,n)*np.sqrt(dt)
+### Settings
+dt = 2**(-6)
+dt_f = 2**(-11)
+n=int(1/dt_f)+1
+x = np.zeros(int(1/dt_f)+1)
+x_true = np.zeros(int(1/dt_f)+1)
+x0 = 1.0
+a = 1.5
+b = 1.0
+t_f = np.arange(0,1+dt_f,dt_f)
+t = np.arange(0,1+dt,dt)
+x_n = 1
+xn = []
+
+###Calculations
+
+##Random Walk
+dW = np.random.normal(0,1,n)*np.sqrt(dt_f)
 W = np.cumsum(dW)
+count = 0
+##Analytical solution
+for i in range(len(t_f)):
+    x_true[i] = x0 * np.exp((a-0.5*b**2)*t_f[i]+b*W[i])  #Analytical 
+    count += dW[i]
+    if (t_f[i] in t):
+        print(t_f[i])
+        x_n = x_n + a*x_n*dt + b*x_n*count              #Approximation
+        xn.append(x_n)
+        count = 0
 
+#%% Plot
+
+##Random Walk
 plt.figure()
 plt.plot(W)
 plt.title("Random Walk")
 
-
-x = np.zeros(int(1/dt)+1)
-x_true = np.zeros(int(1/dt)+1)
-x0 = 1.0
-a = 1.5
-b = 1.0
-
-t = np.arange(0,1+dt,dt)
-
-for i in range(len(t)):
-    #print(i)
-    x_true[i] = x0 * np.exp((a-0.5*b**2)*t[i]+b*W[i])
+##Analytical and approximated solutions
+x1 = np.linspace(0,1,len(x_true))
+x2 = np.linspace(0,1,len(xn))
 
 plt.figure()
-plt.plot(x_true,label="x_true")
-plt.title("x_true")
-
-
-
-#%% Diskrete Loesung
-
-x_n = 1
-xn = []
-
-
-for i in range(0,n):
-    x_n = x_n + a*x_n*dt + b*x_n*dW[i]
-    xn.append(x_n)
-
-#plt.figure()
-plt.plot(xn,"r",label="xn")
+plt.plot(x1,x_true,label="x_true")
+plt.plot(x2,xn,"r",label="xn")
 plt.legend()
-#plt.title("xn")
 
+##Plot Random Numbers dW
 plt.figure()
 plt.plot(dW)
 plt.title("dW")
