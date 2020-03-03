8720205905183c7f53d0a9b8d123485b5afdcfde
Florian Roemer <roemerflo@gmail.com>
2020-03-03 16:01:31 +0100

    modified random number generator and saved rng plots

--- random_number_generator.py
+++ random_number_generator.py
@@ -15,10 +15,12 @@ try:
 except:
     pass
 
+outdir = "Plots/"
 
-#a, b, c= 11, 0, 2**2
 
-a, b, c= 65539, 0, 2**31
+a, b, c= 11, 0, 2**2
+
+#a, b, c= 65539, 0, 2**31
 
 
 n=100000
@@ -39,15 +41,35 @@ for i in range(2,n):
 print(U)
 plt.figure(1)
 plt.hist(U)
+plt.xticks(fontsize=12)
+plt.yticks(fontsize=12)
+plt.xlabel('Value',fontsize=14)
+plt.ylabel('Frequency',fontsize=14)
+plt.title('Histogram of U',fontsize=16)
+plt.savefig(outdir + "Random_number_generator_hist_U_a_"+str(a)+".png", dpi = 300)
 
 plt.figure(2)
 plt.hist(N,bins=100)
+plt.xticks(fontsize=12)
+plt.yticks(fontsize=12)
+plt.xlabel('Value',fontsize=14)
+plt.ylabel('Frequency',fontsize=14)
+plt.title('Histogram of N',fontsize=16)
+plt.savefig(outdir + "Random_number_generator_hist_N_a_"+str(a)+".png", dpi = 300)
 
-plt.figure(3)
-plt.acorr(N)
+
+#plt.figure(3)
+#plt.acorr(N)
 
 plt.figure(4)
 plt.acorr(U-np.mean(U))
+plt.xticks(fontsize=12)
+plt.yticks(fontsize=12)
+plt.xlabel('time lag',fontsize=14)
+plt.ylabel('autocorrelation function',fontsize=14)
+plt.title('Autocorrelation of N',fontsize=16)
+plt.savefig(outdir + "Random_number_generator_autocorr_N_a_"+str(a)+".png", dpi = 300)
 
-plt.figure(5)
-plt.plot(U[1:1000])
+
+#plt.figure(5)
+#plt.plot(U[1:1000])
